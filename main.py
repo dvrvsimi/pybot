0b7d4974b59751dca8436de0b205c8483d7f74b7
dvrvsimi <daraakojede01gmail.com>
2023-04-17 19:11:49 +0100

    testing with openai

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tweepy
-# import openai
+import openai
 
 # calling keys, secrets, tokens from config
 from config import consumer_key, consumer_secret, access_token, access_token_secret, openai_key
-# openai.api_key = openai_key
+openai.api_key = openai_key
 
 
 #Setting credentials to access Twitter API
@@ -14,7 +14,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 #search keyword 
-search = 'blah'
+search = 'hack sultan'
 #maximum limit of tweets to be interacted with
 maxNumberOfTweets = 1
 
@@ -27,7 +27,17 @@ for tweet in tweepy.Cursor(api.search_tweets, search).items(maxNumberOfTweets):
         status = api.get_status(tweet.id)
         if status.retweeted == False and status.favorited == False:
             count = count + 1
-            print("Found tweet by @" + tweet.user.screen_name)
+            print("Found tweet by @/" + tweet.user.screen_name)
             print(count)
     except tweepy.errors.TweepyException as error:
         print(str(error))
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        # {"role": "system", "content": "You are a helpful assistant."},
+        # {"role": "user", "content": input('question 1 ')},
+        {"role": "assistant", "content": ""},
+        {"role": "user", "content": input('user_tweet ')}
+    ]
+)
+print(response['choices'][0]['message']['content'])
