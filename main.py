b8cfb0df57b20c155bfedb2802dd19cd80fe0343
dvrvsimi <daraakojede01gmail.com>
2023-04-25 10:30:48 +0100

    test...

--- main.py
+++ main.py
@@ -14,30 +14,31 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 #search keyword 
-search = 'hack sultan'
+search = []
 #maximum limit of tweets to be interacted with
 maxNumberOfTweets = 1
-
 #to keep track of tweets published
 count = 0
 
 for tweet in tweepy.Cursor(api.search_tweets, search).items(maxNumberOfTweets):
     try:
-        # for each status, overwrite that status by the same status, but from a different endpoint.
         status = api.get_status(tweet.id)
         if status.retweeted == False and status.favorited == False:
             count = count + 1
             print("Found tweet by @/" + tweet.user.screen_name)
+            user_in = ("did" + tweet.user.screen_name + "tweet" + input(search) + "?")
             print(count)
     except tweepy.errors.TweepyException as error:
         print(str(error))
+
+        
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         # {"role": "system", "content": "You are a helpful assistant."},
         # {"role": "user", "content": input('question 1 ')},
         {"role": "assistant", "content": ""},
-        {"role": "user", "content": input('user_tweet ')}
-    ]
+        {"role": "user", "content": tweet.text}
+    ]tests
 )
 print(response['choices'][0]['message']['content'])
